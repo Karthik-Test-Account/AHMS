cc368c786dd8030b7dc367cdc15214f10991170c
Dhanush Keloth <dhanushkeloth@gmail.com>
2024-06-19 21:20:12 +0530

    added logout functionality in dashboard

--- src/components/Sidebar/SidebarLogout/SidebarLogout.jsx
+++ src/components/Sidebar/SidebarLogout/SidebarLogout.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 import LogoutIcon from '@mui/icons-material/Logout';
 import { Box, Button, Typography } from '@mui/material';
+import { getAuth, signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
+
+// const auth = getAuth();
 function SidebarLogout() {
+  const navigate= useNavigate();
+  const handleLogout=()=>{
+    const auth = getAuth();
+    signOut(auth).then(() => {
+      navigate("/")
+    }).catch((error) => {
+      alert("inappropriate logout")
+    });
+  
+  }
   return (
     <Box
       sx={{
@@ -11,7 +26,8 @@ function SidebarLogout() {
         '@media (max-width:600px)':{display:'none'}
       }}
     >
-        <Button variant="contained" href="#" fullWidth sx={{textTransform:'none',marginBottom:'40px',display:'flex',justifyContent:'center',gap:'10px'}}>
+        <Button variant="contained" href="#" fullWidth sx={{textTransform:'none',marginBottom:'40px',display:'flex',justifyContent:'center',gap:'10px'}}
+        onClick={handleLogout}>
             <LogoutIcon />
             <Typography variant="h6">
                 Log Out
