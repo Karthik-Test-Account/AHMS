b44ade767d1caf2ee68bf195a30e32b077f82731
shiva sai <shivasaichouki564@gmail.com>
2024-06-22 16:10:55 +0530

    complaints reupdated

--- src/components/Student Dashboard/complaints/complaints.jsx
+++ src/components/Student Dashboard/complaints/complaints.jsx
@@ -1,35 +1,93 @@
 import React from 'react';
-import { Box, Grid, Toolbar, Typography, FormControl, MenuItem, Select, TextField, Button } from '@mui/material';
+import { Box, Grid, Toolbar, Typography, FormControl, MenuItem, Select, TextField, Button } from "@mui/material";
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import DoneIcon from '@mui/icons-material/Done';
-import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule';
-
 const drawerWidth = 240;
 const headerHeight = 50;
-
-function Complaints() {
+function complaints() {
     const [age, setAge] = React.useState('');
 
     const handleChange = (event) => {
         setAge(event.target.value);
     };
     return (
-        <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
-            <Box component="main" sx={{
-                backgroundColor: '#1B1918', height: `calc(100vh - ${headerHeight}px)`,
-                padding: '15px', marginTop: `${headerHeight}px`, '@media (min-width:600px)': { marginLeft: `${drawerWidth}px ` }
-            }}>
+
+        <div
+            style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}
+        >
+            <Box
+                component="main"
+                sx={{
+                    minHeight: "100vh",
+                    height: "auto",
+                    backgroundColor: "#1B1918",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    padding: "20px 0",
+                    marginTop: `${headerHeight}px`,
+                    "@media (min-width:600px)": { marginLeft: `${drawerWidth}px` },
+                }}
+            >
                 <Toolbar />
-                <Grid container sx={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'row', gap: '10px' }}>
+                <Grid
+                    container
+                    gap={2}
+                    sx={{
+                        width: "70%",
+                        color: "white",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "start",
+                    }}
+                >
                     <Grid item>
-                        <Typography variant='h4' sx={{ fontWeight: 'bold', color: 'white' }}>
+                        <Typography variant="h3" fontWeight={600} align="center">
                             Complaints
                         </Typography>
                     </Grid>
-                    <Grid item sx={{ width: '100%', height: '100%' }}>
-                        <Grid container sx={{ width: '70%', height: '50%', display: 'flex', justifyContent: 'column', color: 'white', gap: '5%', marginLeft: '17%' }}>
-                            <Grid item sx={{ height: { lg: 'auto', md: 'auto', sm: 'auto', xs: 'auto' }, width: { lg: '45%', md: '100%', sm: '100%', xs: '100%' }, padding: '30px', backgroundColor: 'black', overflow: 'auto', borderRadius: '7px', '@media (max-width:1136px)': { width: '100%', height: 'auto' } }}>
-                                <Grid container gap={2} p={2} sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <Grid item sx={{ width: "100%" }}>
+                        <Grid
+                            container
+                            gap={1}
+                            sx={{
+                                width: "100%",
+                                padding: "5px",
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "space-evenly",
+                                flexDirection: 'row',
+
+                                "@media (max-width:1100px)": { flexDirection: "column" },
+                            }}
+                        >
+                            <Grid
+                                item
+                                sx={{
+                                    backgroundColor: "#080808",
+                                    width: "53%",
+                                    minHeight: "50vh",
+                                    padding: '10px',
+                                    height: "auto",
+                                    "@media (max-width:1100px)": { width: "100%" },
+                                    borderRadius: "8px",
+                                }}
+                            >
+                                <Grid
+                                    container
+                                    sx={{
+                                        width: "100%",
+                                        display: "flex",
+                                        flexDirection: "column",
+                                        justifyContent: "start",
+                                        padding: "15px",
+                                        height: "45vh",
+                                        overflowY: "auto",
+                                        flexWrap: "revert",
+                                        "&::-webkit-scrollbar": { display: "none" },
+                                    }}
+                                >
                                     <Grid item>
                                         <Typography variant='h6' sx={{ fontWeight: 'bold', }}>
                                             Your complaint type
@@ -87,65 +145,178 @@ function Complaints() {
                                     </Grid>
                                 </Grid>
                             </Grid>
-                            <Grid item sx={{ height: { lg: 'auto', md: 'auto', sm: 'auto', xs: 'auto' }, width: { lg: '45%', md: '100%', sm: '100%', xs: '100%' }, padding: '30px', backgroundColor: 'black', borderRadius: '7px' }}>
-                                <Grid container gap={2} p={2} sx={{ display: 'flex', flexDirection: 'column', gap: '30px', '@media (max-width:1136px)': { height: '100%', gap: '1.5px' } }}>
+
+
+                            <Grid
+                                item
+                                sx={{
+                                    backgroundColor: "#080808",
+                                    width: "45%",
+                                    minHeight: "50vh",
+                                    padding: '10px',
+                                    height: "auto",
+                                    "@media (max-width:1100px)": { width: "100%" },
+                                    borderRadius: "8px",
+                                }}
+                            >
+                                <Grid
+                                    container
+                                    sx={{
+                                        width: "100%",
+                                        display: "flex",
+                                        flexDirection: "column",
+                                        justifyContent: "start",
+                                        padding: "15px",
+                                        height: "45vh",
+                                        overflowY: "auto",
+                                        flexWrap: "revert",
+                                        gap: '10px',
+                                        "&::-webkit-scrollbar": { display: "none" },
+                                    }}
+                                >
                                     <Grid item>
-                                        <Typography variant='h5' sx={{ fontWeight: 'bold' }}>
-                                            Registered Complaints
+                                        <Typography variant='h6' sx={{ fontWeight: 'bold', }}>
+                                            Registered complaints
                                         </Typography>
                                     </Grid>
-                                    <Grid container sx={{ display: 'flex', flexDirection: 'column' }}>
-                                        <Grid container sx={{ display: 'flex', flexDirection: 'column', }}>
-                                            <Grid conatiner sx={{ display: 'flex', flexDirection: 'row', gap: '20px' }}>
-                                                <Grid item >
-                                                    <DoneIcon sx={{ fontSize: '30px' }} />
+                                    <Grid container sx={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+                                        <Grid
+                                            item
+                                            sx={{
+                                                width: "100%",
+                                                display: "flex",
+                                                alignItems: "center",
+                                                justifyContent: "space-between",
+                                                padding: "5px",
+                                            }}
+                                        >
+                                            {/* start of box */}
+                                            <Grid
+                                                container
+                                                sx={{
+                                                    width: "60%",
+                                                    display: "flex",
+                                                    justifyContent: "start",
+                                                    alignItems: "center",
+                                                    gap: "10px",
+                                                }}
+                                            >
+                                                <Grid item>
+                                                    <DoneIcon />
                                                 </Grid>
                                                 <Grid item>
-                                                    <Typography variant='body1' sx={{}}>AC</Typography>
-                                                </Grid>
-                                                <Grid container sx={{ display: 'flex', justifyContent: 'end' }}>
-                                                    <Typography variant='body1' sx={{}}>Electric</Typography>
+                                                    <Grid
+                                                        container
+                                                        sx={{
+                                                            display: "flex",
+                                                            flexDirection: "column",
+                                                            alignItems: "start",
+                                                        }}
+                                                    >
+                                                        <Grid item>
+                                                            <Typography sx={{ fontSize: "13px" }}>
+                                                                AC
+                                                            </Typography>
+                                                        </Grid>
+                                                        <Grid item>
+                                                            <Typography sx={{ fontSize: "13px" }}>
+                                                                May 21, 2023
+                                                            </Typography>
+                                                        </Grid>
+                                                    </Grid>
                                                 </Grid>
                                             </Grid>
-                                            <Grid item sx={{ marginLeft: '50px', marginTop: '-20px', '@media (max-width:1136px)': { marginLeft: '40px', marginTop: '-10px' } }}>
-                                                <Typography variant='caption' sx={{ color: 'gray', '@media (max-width:1136px)': { fontSize: '10px' } }}>May 20,2023</Typography>
+                                            <Grid
+                                                container
+                                                sx={{
+                                                    width: "calc(40% - 10px)",
+                                                    display: "flex",
+                                                    alignItems: "center",
+                                                    justifyContent: "end",
+                                                }}
+                                            >
+                                                <Grid item>
+                                                    <Typography sx={{ fontSize: "14px" }}>
+                                                        Rs. 9000
+                                                    </Typography>
+                                                </Grid>
                                             </Grid>
+                                            {/* end of box */}
                                         </Grid>
-                                        <Grid item sx={{ margin: '30px', '@media (max-width:1136px)': { margin: '10px' } }}>
-                                            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                                                <Box sx={{ borderBottom: '1px solid', width: '100%', color: 'gray' }} />
-                                            </Box>
-                                        </Grid>
-                                        <Grid container sx={{ display: 'flex', flexDirection: 'column' }}>
-                                            <Grid conatiner sx={{ display: 'flex', flexDirection: 'row', gap: '20px' }}>
-                                                <Grid item >
-                                                    <DoneIcon sx={{ fontSize: '30px' }} />
+                                        <Grid
+                                            item
+                                            sx={{
+                                                width: "100%",
+                                                display: "flex",
+                                                alignItems: "center",
+                                                justifyContent: "space-between",
+                                                padding: "5px",
+                                            }}
+                                        >
+                                            {/* start of box */}
+                                            <Grid
+                                                container
+                                                sx={{
+                                                    width: "60%",
+                                                    display: "flex",
+                                                    justifyContent: "start",
+                                                    alignItems: "center",
+                                                    gap: "10px",
+                                                }}
+                                            >
+                                                <Grid item>
+                                                    <DoneIcon />
                                                 </Grid>
                                                 <Grid item>
-                                                    <Typography variant='body1' sx={{}}>AC</Typography>
-                                                </Grid>
-                                                <Grid container sx={{ display: 'flex', justifyContent: 'end' }}>
-                                                    <Typography variant='body1' sx={{}}>Electric</Typography>
+                                                    <Grid
+                                                        container
+                                                        sx={{
+                                                            display: "flex",
+                                                            flexDirection: "column",
+                                                            alignItems: "start",
+                                                        }}
+                                                    >
+                                                        <Grid item>
+                                                            <Typography sx={{ fontSize: "13px" }}>
+                                                                Water
+                                                            </Typography>
+                                                        </Grid>
+                                                        <Grid item>
+                                                            <Typography sx={{ fontSize: "13px" }}>
+                                                                May 21, 2023
+                                                            </Typography>
+                                                        </Grid>
+                                                    </Grid>
                                                 </Grid>
                                             </Grid>
-                                            <Grid item sx={{ marginLeft: '50px', marginTop: '-20px', '@media (max-width:1136px)': { marginLeft: '40px', marginTop: '-10px' } }}>
-                                                <Typography variant='caption' sx={{ color: 'gray' }}>May 20,2023</Typography>
+                                            <Grid
+                                                container
+                                                sx={{
+                                                    width: "calc(40% - 10px)",
+                                                    display: "flex",
+                                                    alignItems: "center",
+                                                    justifyContent: "end",
+                                                }}
+                                            >
+                                                <Grid item>
+                                                    <Typography sx={{ fontSize: "14px" }}>
+                                                        Rs. 9000
+                                                    </Typography>
+                                                </Grid>
                                             </Grid>
-                                        </Grid>
-                                        <Grid item sx={{ margin: '30px' }}>
-                                            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                                                <Box sx={{ borderBottom: '1px solid', width: '100%', color: 'gray' }} />
-                                            </Box>
+                                            {/* end of box */}
                                         </Grid>
                                     </Grid>
                                 </Grid>
                             </Grid>
+
+
                         </Grid>
                     </Grid>
                 </Grid>
             </Box>
         </div >
-    );
+    )
 }
 
-export default Complaints;
+export default complaints
