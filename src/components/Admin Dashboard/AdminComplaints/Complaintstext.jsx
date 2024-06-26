e5298b0f0dcca5348458962ac4321e0487ef01e1
shiva sai <shivasaichouki564@gmail.com>
2024-06-26 21:08:20 +0530

    admin complaints updatedNew

---
+++ src/components/Admin Dashboard/AdminComplaints/Complaintstext.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Typography, Box, Toolbar, Grid } from "@mui/material";
+import ErrorOutlineRoundedIcon from "@mui/icons-material/ErrorOutlineRounded";
+function Complaintstext(props) {
+  return (
+    <>
+      <Grid
+        item
+        sx={{
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Grid
+          container
+          sx={{
+            width: "50%",
+            display: "flex",
+            justifyContent: "start",
+            alignItems: "center",
+            gap: "10px",
+          }}
+        >
+          <Grid item sx={{ color: "white" }}>
+            {props.lefticon}
+          </Grid>
+          <Grid item>
+            <Grid
+              container
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "start",
+              }}
+            >
+              <Grid item>
+                <Typography sx={{ fontSize: "13px", color: "white" }}>
+                  {props.title}
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography sx={{ fontSize: "13px", color: "white" }}>
+                  {props.description}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid
+          container
+          sx={{
+            width: "calc(50% - 10px)",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "end",
+          }}
+        >
+          <Grid item sx={{ color: "white" }}>
+            {props.righticon}
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
+}
+
+export default Complaintstext;
